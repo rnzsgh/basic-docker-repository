2f9c2e10e8265109ecf6cf2ed80e67c0419204aa
Ryan <ryannitz@amazon.com>
2019-06-04 21:17:08 -0400

    Initial

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+STACK_NAME=docker-a
+
+ACCOUNT_ID=$(aws sts get-caller-identity --output text --query 'Account')
+
+ENV_NAME=dev
+
+KEY_NAME=SOMETHING
+
+HOSTNAME=some.domain.com
+
+CERT_ARN=arn:aws:acm:us-east-1:$ACCOUNT_ID:certificate/44147128-b634-438f-85af-95dc39a1f0a4
+
+aws cloudformation create-stack \
+  --stack-name $STACK_NAME \
+  --template-body file://stack.cfn.yml \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameters \
+  ParameterKey=CertificateArn,ParameterValue=$CERT_ARN \
+  ParameterKey=KeyName,ParameterValue=$KEY_NAME \
+  ParameterKey=RegistryHostname,ParameterValue=$HOSTNAME
+
+

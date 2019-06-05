9cc0d0d7b1520bd24634212bb1ce9074b421b46a
Ryan <ryannitz@amazon.com>
2019-06-04 21:54:45 -0400

    Add secret param

--- test.sh
+++ test.sh
@@ -12,6 +12,9 @@ HOSTNAME=some.domain.com
 
 CERT_ARN=arn:aws:acm:us-east-1:$ACCOUNT_ID:certificate/44147128-b634-438f-85af-95dc39a1f0a4
 
+# Change this!
+SECRET=2382SdUfjsdfjFJKSq
+
 aws cloudformation create-stack \
   --stack-name $STACK_NAME \
   --template-body file://stack.cfn.yml \
@@ -19,6 +22,7 @@ aws cloudformation create-stack \
   --parameters \
   ParameterKey=CertificateArn,ParameterValue=$CERT_ARN \
   ParameterKey=KeyName,ParameterValue=$KEY_NAME \
-  ParameterKey=RegistryHostname,ParameterValue=$HOSTNAME
+  ParameterKey=RegistryHostname,ParameterValue=$HOSTNAME \
+  ParameterKey=RegistrySecret,ParameterValue=$SECRET
 
 
